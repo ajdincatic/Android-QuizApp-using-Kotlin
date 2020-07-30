1f31a1faf299d00f2f16bc6cd7e7e6ff9dcc6c10
Ajdin Ćatić <ajdin.catic@edu.fit.ba>
2020-07-30 16:46:54 +0200

    init

---
+++ Basic Kotlin QuizApp/app/src/main/java/ba/fit/quizapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package ba.fit.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btnStart.setOnClickListener {
+            if(etName.text.toString().isEmpty()){
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
